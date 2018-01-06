edc3002d8afc447871c543885e6a1a2f345cb0cb
Ryan Grippeling <R.Grippeling@hotmail.com>
2018-01-06 16:08:52 +0100

    After third video - Added decrement button - Removed superfluous code - Added some comments to explain process - Added a watcher to more efficiently run tests while devving

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,7 +1,8 @@
 <template>
     <div>
         <span class="count" v-text="count"></span>
-        <button @click="count++">Increment</button>
+        <button class="increment" @click="count++">Increment</button>
+        <button class="decrement" @click="count > 0 && count--" v-show="count > 0">Decrement</button>
     </div>
 </template>
 
--- test/counter.spec.js
+++ test/counter.spec.js
@@ -13,13 +13,33 @@ describe('Counter', () => {
         expect(wrapper.vm.count).toBe(0);
     });
 
-    it('increments the count when the button is clicked', () => {
-        expect(wrapper.vm.count).toBe(0);
-        wrapper.find('button').trigger('click');
+    it('increments the count when the increment button is clicked', () => {
+        wrapper.find('.increment').trigger('click');
         expect(wrapper.vm.count).toBe(1);
     });
 
-    it('present the current count', () => {
+    it('decrements the count when the decrement button is clicked', () => {
+        wrapper.setData({ count: 5 });
+
+        wrapper.find('.decrement').trigger('click');
+        expect(wrapper.vm.count).toBe(4);
+    });
+
+    it('cannot be decreased to less than zero', () => {
+        // Check if the buton is hidden
+        expect(wrapper.vm.count).toBe(0);
+        expect(wrapper.find('.decrement').hasStyle('display', 'none')).toBe(true);
+
+        // Check if even if the decrement button's click event is programatically triggeren, that it doesn't do this
+        wrapper.find('.decrement').trigger('click');
+        expect(wrapper.vm.count).toBe(0);
+
+        // Check if button reappears if the count is set to a number higher than 0
+        wrapper.setData({ count: 4 });
+        expect(wrapper.find('.decrement').hasStyle('display', 'none')).toBe(false);
+    });
+
+    it('presents the current count', () => {
         expect(wrapper.find('.count').html()).toContain(0);
         wrapper.find('button').trigger('click');
         expect(wrapper.find('.count').html()).toContain(1);
