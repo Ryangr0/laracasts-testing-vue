6403b74d1e80f180f490692b5e23a9dd95502d43
Ryan Grippeling <R.Grippeling@hotmail.com>
2018-01-07 12:45:54 +0100

    After seventh video - Added Question tests - some helper methods

---
+++ src/components/Question.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <!-- Viewing the question -->
+        <div v-if="!editing">
+            <h1 v-text="question.title"></h1>
+            <div class="body" v-text="question.body"></div>
+            <button id="edit" @click="editing = !editing">Edit</button>
+        </div>
+
+        <!-- Editing the question -->
+        <div v-if="editing">
+            <input type="text" name="title" v-model="form.title">
+            <textarea name="body" v-model="form.body"></textarea>
+
+            <button id="update" @click="update">Update</button>
+            <button id="cancel" @click="cancel">Cancel</button>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['dataQuestion'],
+        data() {
+            return {
+                question: this.dataQuestion,
+                form: {
+                    title: this.dataQuestion.title,
+                    body: this.dataQuestion.body
+                },
+                editing: false
+            }
+        },
+        methods: {
+            update() {
+                this.question = this.form;
+                this.editing = false;
+            },
+            cancel() {
+                this.editing = false;
+            }
+        }
+    }
+</script>
---
+++ test/Question.spec.js
@@ -0,0 +1,76 @@
+import { mount } from 'vue-test-utils';
+import expect from 'expect';
+import Question from '../src/components/Question.vue';
+
+describe('Question', () => {
+    let wrapper;
+    beforeEach(() => {
+        wrapper = mount(Question, {
+            propsData: {
+                dataQuestion: {
+                    title: 'The title',
+                    body: 'The body'
+                }
+            }
+        });
+    });
+
+    it('presents the title and the body', () => {
+        see('The title', 'h1');
+        see('The body', '.body');
+    });
+
+    it('can be edited', () => {
+        expect(wrapper.contains('input[name=title]')).toBe(false);
+        wrapper.find('#edit').trigger('click');
+
+        expect(wrapper.find('input[name=title]').element.value).toBe('The title');
+        expect(wrapper.find('textarea[name=body]').element.value).toBe('The body');
+    });
+
+    it('should hide the edit button during edit mode', () => {
+        click('button#edit');
+        expect(wrapper.contains('button#edit')).toBe(false);
+    });
+
+    it('should update the question after being edited', () => {
+        click('button#edit');
+
+        type('input[name=title]', 'New title');
+        type('textarea[name=body]', 'New body');
+
+        click('button#update');
+
+        expect(wrapper.vm.editing).toBe(false);
+
+        see('New title', 'h1');
+        see('New body', '.body');
+    });
+
+    it('should be able to cancel out of edit mode', function () {
+        click('button#edit');
+
+        type('input[name=title]', 'New title');
+        type('textarea[name=body]', 'New body');
+
+        click('button#cancel');
+
+        see('The title', 'h1');
+        see('The body', '.body');
+    });
+
+    let see = (string, selector) => {
+        let wrap = selector ? wrapper.find(selector) : wrapper;
+        return expect(wrap.html()).toContain(string);
+    };
+
+    let type = (selector, string) => {
+        let wrap = wrapper.find(selector);
+        wrap.element.value = string;
+        wrap.trigger('input');
+    };
+
+    let click = selector => {
+        return wrapper.find(selector).trigger('click');
+    };
+});
